a1517a18983d6d8aeba21e8522c043c38f5fef61
Tumworobere <annaht.annah@gmail.com>
2024-03-23 16:02:18 +0400

    Initialize project using Create React App

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {LandingPageHeader}  from './components/Navigations/LandingPageHeader'
+import {HeroPage} from './components/HeroPage/HeroPage';
+
+function App() {
+  return (
+    <div>
+      <LandingPageHeader />
+      <HeroPage />
+    </div>
+  );
+}
+
+export default App;
